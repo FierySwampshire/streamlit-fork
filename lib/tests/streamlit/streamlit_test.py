188e85f4c1bef1bcf9dc3870bbd23c8456056806
Tomek Szerszeń <tomasz.szerszen@snowflake.com>
2023-02-02 14:08:43 +0100

    Add support for tooltips on text elements

--- lib/tests/streamlit/streamlit_test.py
+++ lib/tests/streamlit/streamlit_test.py
@@ -424,17 +424,6 @@ class StreamlitAPITest(DeltaGeneratorTestCase):
             el.doc_string.doc_string.startswith("Display text in header formatting.")
         )
         self.assertEqual(el.doc_string.type, "<class 'method'>")
-        if sys.version_info < (3, 9):
-            # Python < 3.9 represents the signature slightly differently
-            self.assertEqual(
-                el.doc_string.signature,
-                "(body: object, anchor: Union[str, NoneType] = None) -> 'DeltaGenerator'",
-            )
-        else:
-            self.assertEqual(
-                el.doc_string.signature,
-                "(body: object, anchor: Optional[str] = None) -> 'DeltaGenerator'",
-            )
 
     def test_st_info(self):
         """Test st.info."""
